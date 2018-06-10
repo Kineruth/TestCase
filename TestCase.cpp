d64c5741af9c6398aa8ac18d15cceb611269373b
Kineret Ruth <naharykin@gmail.com>
2018-06-10 21:04:43 +0000

    Fixed errors and moced some functions to cpp file

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,1 +1,12 @@
 #include "TestCase.hpp"
+
+TestCase::TestCase(string name, ostream& c) : c(c.rdbuf()) {
+            _name = name;
+            total = passed = failed = 0;
+}
+
+void TestCase::print(){
+    
+    c << _name << ": " << failed << " failed, " << passed << " passed, " << total << " total." << endl;
+    c << "---" << endl;
+}
--- TestCase.hpp
+++ TestCase.hpp
@@ -2,78 +2,96 @@
 
 #include <sstream>
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 
 class TestCase{
     
     private:
-        string name;
-        ostream c;
         int total, passed, failed;
+        string _name;
+        ostream c;
+        
+        
         
     public:
-        TestCase(string name, ostream& c):c(c.rdbuf()){
-            this->name = name;
-            total = passed = failed = 0;
-        }
+    
+        TestCase(string name, ostream& c);
+        void print();
         
         template <typename T> TestCase& check_equal(const T a, const T b){
-            this->total++;
-            if(a!=b){
-              c<<this->name<<": Failure in test #"<< total << a <<": should equal "<< b <<endl;
-              failed++;
-            } else{
-               passed++;
+            
+            total++;
+            
+            if(a == b){
+                passed++;
+            } 
+            else{
+                c << _name << ": Failure in test #" <<  total << a << ": should equal " << b << endl;
+                failed++;
             }
+            
             return *this;
         } 
         
         template <typename T> TestCase& check_different (const T a, const T b){
-            this->total++;
-            if (a==b){
-                c<<this->name<<": Failure in test #"<< total << a <<": should different "<< b <<endl;
-                failed++;
-            } else{
+            
+            total++;
+            
+            if (a != b){
                 passed++;
+            } 
+            else{
+                c << _name << ": Failure in test #" << total << a << ": should different " << b << endl;
+                failed++;
             }
+            
             return *this;
         } 
+        
         /*
         we need another tamplate variable in orfer to execute differents options on him.
         
         */
-        template<typename Fun , typename T>
-        TestCase& check_function(Fun func,const T a, const int b){
-            int res = func(a);
-            this->total++;
-            if(res!=b){
-                c<<this->name<<": Failure in test #"<< total << ": Function should return "<< b <<" but is "<<res<<"!"<<endl;
+        template<typename Fun , typename T> TestCase& check_function(Fun func,const T a, const int b){
+            
+            total++;
+            int res = (*func)(a);
+            
+            if(res == b){
+                passed++;
+            } 
+            else{
+               c << _name << ": Failure in test #" << total << ": Function should return " << b << " but is " << res << "!" << endl;
                 failed++;
-            } else{
-               passed++;
             }
+            
             return *this;
         } 
         
         
         template <typename T> TestCase& check_output(T a,string str){
-            this->total++;
+            
+            total++;
             ostringstream ss;
             ss << a;
-            if(ss.str() != str){
-                c<<this->name<<": Failure in test #"<< total << ": string value should be "<< str <<" but returned "<< a <<"!"<<endl;
+            
+            if(ss.str() == str){
+                passed++;
+            } 
+            else{
+               c << _name << ": Failure in test #" << total << ": string value should be " << str << " but returned " << a << "!" << endl;
                 failed++;
-            } else{
-               passed++;
             }
+            
             return *this;
         }
-        TestCase& print(){
-            c << name << ": " << failed << " failed, " << passed << " passed, " << total << " total." << endl;
-            // c << "---" << endl;
-            return *this;
-        }
+        
+        
+        // TestCase& print(){
+            
+        //     c << _name << ": " << failed << " failed, " << passed << " passed, " << total << " total." << endl;
+        //     c << "---" << endl;
+        //     return *this;
+        // }
 };
